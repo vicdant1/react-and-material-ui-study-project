e951fe3f2f236fc6432389b67d47c9d27120d297
vicdant1 <dantjv@outlook.com>
2022-01-27 16:36:17 -0300

    working on short contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,8 +1,20 @@
-const Contact = () => { 
-    return(
-        <div className="page">
-            Contact
-        </div>
-    )
-}
-export default Contact
+import profile from "../../img/profile.svg";
+import { Box, Typography } from "@material-ui/core";
+import useStyles from "./contactStyles";
+
+const Contact = () => {
+  const classes = useStyles();
+
+  return (
+    <div className="page">
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <img className={classes.profileSvg} src={profile} alt="profile-theme" />
+      </Box>
+      <br/>
+      <Typography variant="h3" align="center">
+        Coming soon...
+      </Typography>
+    </div>
+  );
+};
+export default Contact;
---
+++ src/pages/Contact/contactStyles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(() => ({
+  profileSvg: {
+    display: "block",
+    width: "80%"
+  },
+}));
+
+export default useStyles
