d5f79ff267cd70b48475edea195ccf970c8a5b42
vicdant1 <dantjv@outlook.com>
2022-01-27 16:41:35 -0300

    working on short support page

--- src/pages/Support/Support.jsx
+++ src/pages/Support/Support.jsx
@@ -1,9 +1,20 @@
-const Support = () => {
-    return(
-        <div className="page">
-            Support
-        </div>
-    )
-}
+import planet from '../../img/planet.svg'
+import { Box, Typography } from '@material-ui/core';
+import useStyles from './supportStyles'
 
-export default Support
+const Support = () => {
+  const classes = useStyles();
+  return (
+    <div className="page">
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <img className={classes.profileSvg} src={planet} alt="profile-theme" />
+      </Box>
+      <br />
+      <Typography variant="h3" align="center">
+        Coming soon...
+      </Typography>
+    </div>
+  );
+};
+
+export default Support;
---
+++ src/pages/Support/supportStyles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(() => ({
+  profileSvg: {
+    display: "block",
+    width: "60%",
+  },
+}));
+
+export default useStyles;
